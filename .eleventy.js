436e68417fa1adf101e751bf90f8d60bfed879db
Chris Uehlinger <chris.uehlinger@gmail.com>
2020-07-17 13:39:29 -0400

    Initial commit

---
+++ .eleventy.js
@@ -0,0 +1,9 @@
+module.exports = function(eleventyConfig) {
+  eleventyConfig.addLayoutAlias('default', 'layouts/default.html');
+  return {
+    dir: {
+      input: "./source",      // Equivalent to Jekyll's source property
+      output: "./_site" // Equivalent to Jekyll's destination property
+    }
+  };
+};
